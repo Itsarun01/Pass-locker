fe8483ddaee663db48affac725e1f6514b05d38e
Zodik09 <adarshvishwakarma09nov2k@gmail.com>
2024-06-28 11:43:05 +0530

    Added the styles to the whole website.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
   return (  
-    <nav className="bg-slate-100 ">
-      <div className='my-container flex justify-between items-center px-4 py-8 h-14 '>
-           <div className="logo font-bold">Pass Locker</div>
+    <nav className="bg-slate-100">
+      <div className='my-container flex justify-between items-center px-4 py-8 h-14 w-[80vw]'>
+           <div className="logo font-bold text-xl">Pass Locker</div>
              <ul>
-              <li className="flex gap-6"> 
-                <a className="hover:border-b-2 border-indigo-600" href='#'>Home</a>
-                <a className="hover:border-b-2 border-indigo-600" href='#'>About</a>
-                <a className="hover:border-b-2 border-indigo-600" href='#'>Contact</a>
+              <li className="flex gap-6 relative"> 
+                <a className="hover:border-b-2 border-red-600 transition-all ease-linear duration-200" href='#'>Home</a>
+                <a className="hover:border-b-2 border-red-600 transition-all ease-linear duration-200" href='#'>About</a>
+                <a className="hover:border-b-2 border-red-600 transition-all ease-linear duration-200" href='#'>Contact</a>
               </li>
            </ul> 
            </div>
