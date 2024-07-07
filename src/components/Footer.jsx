830bf0c67d1f461ef4f79a578456beb5bb5a605b
Itsarun01 <hey.arunkumar05@gmail.com>
2024-07-07 22:08:41 +0530

    chnage bg and also some chnages in foooter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,17 +4,10 @@ const footer = () => {
   return (
     <>
       <footer className="rounded-lg shadow bg-red-500">
-        <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-          <span className=" text-white font-bold">
-            © 2024{" "}
-            <a href="https://flowbite.com/" class="hover:underline">
-              Pass Locker
-            </a>
-            . <span className="text-black">All Rights Reserved.</span>
-          </span>
-          <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-white sm:mt-0 ">
+        <div className="w-full mx-auto p-4 ">
+          <ul className=" text-center  text-sm font-medium text-white ">
             <li>
-              Made By{" "}
+              Made By : {""}
               <a
                 className="text-white hover:text-black"
                 href="https://github.com/Itsarun01"
